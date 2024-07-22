7cfb117571d7902c9a623e4c3285d64e57ac79a9
Raed Nimer <raed.nimer7@gmail.com>
2024-07-22 23:37:10 +0000

    Add formatted data to javascript file

--- project.js
+++ project.js
@@ -10,3 +10,32 @@ let inputElement = document.getElementById("city-input")
 let searchElement = document.getElementById("search-btn")
 let imageElement = document.getElementById("image")
 let weatherdescriptionElement = document.getElementById("weather-description")
+
+
+let apiCall = async (city) => {
+    
+    //API Call
+    //URL
+    try {
+        let API_KEY = "ca62d981f998767dd6c8395950bb7f3d"
+        let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY
+        console.log(url);
+
+        let response = await fetch(url)
+        let formattedData = await response.json()
+        console.log(formattedData);
+        //If the city is not found...
+        if(formattedData.cod === '404'){
+            alert("City not found")
+        } else { //city found
+   
+    
+    
+     let place = formattedData.name
+
+    locationElement.innerHTML = "City:" + place
+
+    let iconcode = formattedData.weather[0].icon
+
+    let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    
