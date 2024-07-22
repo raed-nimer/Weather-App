872fc46084b512e769c3a38aaec0fde92afeee9c
Raed Nimer <raed.nimer7@gmail.com>
2024-07-22 23:05:08 +0000

    Add elements from html to js

---
+++ project.js
@@ -0,0 +1,4 @@
+let tempElement = document.getElementById("temp")
+let locationElement = document.getElementById("location")
+let feelsLikeElement = document.getElementById("feels-like")
+let humidityElement = document.getElementById("humidity")
