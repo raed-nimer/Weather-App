b31dc68d3cbcdacb996b44b0820637d1331fd88a
Raed Nimer <raed.nimer7@gmail.com>
2024-08-20 19:07:19 +0000

    Add comments and improvements

--- js/maps.js
+++ js/maps.js
@@ -1,105 +1,115 @@
-let searchBtnElement = document.getElementById("search-country-btn")
-let countryFlagELement = document.getElementById("country-flag-container")
-let countryNameELement = document.getElementById("country-name")
-let searchInputElement = document.getElementById("country-input")
-let capitalElement = document.getElementById("capital")
-let languageElement = document.getElementById("language")
-let populationELement = document.getElementById("population")
-let timezoneElement = document.getElementById("timezone")
- let continentElement = document.getElementById("continent")
-let regionElement = document.getElementById("region")
-let countriesInformation = []
-var map = L.map('map').setView([0, 0], 1);
-let marker = null
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=p14qZB5wwA8Y5xetwUyR', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-  }).addTo(map);
-let fetchCountryInfo = async () => {
-    
-    //API Call
-    //URL
-    try {
-        let url = "https://restcountries.com/v3.1/all" 
+// Html elements
+let searchBtnElement = document.getElementById("search-country-btn");
+let countryFlagELement = document.getElementById("country-flag-container");
+let countryNameELement = document.getElementById("country-name");
+let searchInputElement = document.getElementById("country-input");
+let capitalElement = document.getElementById("capital");
+let languageElement = document.getElementById("language");
+let populationELement = document.getElementById("population");
+let timezoneElement = document.getElementById("timezone");
+let continentElement = document.getElementById("continent");
+let regionElement = document.getElementById("region");
+// Array to hold all countries information
+let countriesInformation = [];
+// Adding leaflet.js map
+var map = L.map("map").setView([0, 0], 1);
+let marker = null;
+L.tileLayer(
+  "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=p14qZB5wwA8Y5xetwUyR",
+  {
+    attribution:
+      '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+  }
+).addTo(map);
 
-        let response = await fetch(url)
-        let formattedData = await response.json()
-        console.log(formattedData);
-        countriesInformation = [...formattedData]
-        let countryNames = formattedData.map((country) => country.name.common)
-        console.log("countries: ", countryNames);
-        countryNames.sort()
-        console.log("countries after: ", countryNames);
-        let output = ""
-        for (let i = 0; i < countryNames.length; i++) {
-            output += "<option>"+countryNames[i]+"</option>"
-            
-        }
-        
-        searchInputElement.innerHTML = output 
-        let country = countriesInformation.find((c) => c.name.common === countryNames[0])
-        // setting first country as default
-        populateData(country)
-       
-    } 
- catch(error){
+// Method to fetch all countries information
+let fetchCountryInfo = async () => {
+  //API Call
+  //URL
+  try {
+    // API ENDPOINT
+    let url = "https://restcountries.com/v3.1/all";
+    // API CALL
+    let response = await fetch(url);
+    let formattedData = await response.json();
+    console.log(formattedData);
+    countriesInformation = [...formattedData];
+    // Saving countries names into an array
+    let countryNames = formattedData.map((country) => country.name.common);
+    console.log("countries: ", countryNames);
+    // Sorting country name alphabetically
+    countryNames.sort();
+    console.log("countries after: ", countryNames);
+    // Creating options list for select based on country names
+    let output = "";
+    for (let i = 0; i < countryNames.length; i++) {
+      output += "<option>" + countryNames[i] + "</option>";
+    }
+
+    searchInputElement.innerHTML = output;
+    let country = countriesInformation.find(
+      (c) => c.name.common === countryNames[0]
+    );
+    // setting first country as default
+    populateData(country);
+  } catch (error) {
     console.log("We are inside catch block");
     console.log(error.message);
     // if anything goes wrong in the 'try' block,
     // it will jump to catch()
+  }
+};
 
-}
-}
+// Method to show/populate data
 let populateData = (country) => {
-    console.log(country);
-    countryNameELement.innerHTML = country.name.common
-    countryFlagELement.innerHTML = "<img src='"+ country.flags.png +"'/>"
-    capitalElement.innerHTML = country.capital[0]
-    populationELement.innerHTML = country.population 
-    regionElement.innerHTML = country.region
-    timezoneElement.innerHTML = country.timezones.join(", ")
-    languageElement.innerHTML = Object.values(country.languages).join(", ")
-    continentElement.innerHTML = country.continents.join(", ")
-    console.log(country.flag);
+  //   Displaying country name
+  countryNameELement.innerHTML = country.name.common;
+  //  Displaying country flag
+  countryFlagELement.innerHTML = "<img src='" + country.flags.png + "'/>";
+  capitalElement.innerHTML = country.capital[0];
+  populationELement.innerHTML = country.population;
+  regionElement.innerHTML = country.region;
+  timezoneElement.innerHTML = country.timezones.join(", ");
+  languageElement.innerHTML = Object.values(country.languages).join(", ");
+  continentElement.innerHTML = country.continents.join(", ");
+  console.log(country.flag);
+  // Getting latitude and longitude
+  let latitude = country.latlng[0];
+  let longitude = country.latlng[1];
 
-    let latitude = country.latlng[0]
-    let longitude = country.latlng[1]
+  // Removing old marker
+  if (marker != null) {
+    map.removeLayer(marker);
+  }
+  // Jumping to current location
+  //   map.setView([latitude, longitude], 10)
+  map.flyTo([latitude, longitude], 5);
+  marker = L.marker([latitude, longitude]).addTo(map);
+};
 
-      // Removing old marker
-      if (marker != null) {
-        map.removeLayer(marker)
-    }
-    // Jumping to current location
- //   map.setView([latitude, longitude], 10)
- map.flyTo([latitude, longitude], 5);
- marker = L.marker([latitude, longitude]).addTo(map);
-    
-}
-
-fetchCountryInfo()
+fetchCountryInfo();
 //Change to handleClick()
 const handleButtonClick = () => {
-    let value = searchInputElement.value //this will give the value
-    console.log(value)
-    if (value !== ""){
-}
-    
-}
-    const handleSelectChange = () => {
-        let value = searchInputElement.value
-        console.log(value);
-        let country = null
-        for (let i = 0; i < countriesInformation.length; i++) {
-            if (countriesInformation[i].name.common == value) {
-                console.log(countriesInformation[i]);
-                country = countriesInformation[i]
-                break
-                
-            }
-            
-        }
-        // Passing country to populateData function
-        populateData(country)
+  let value = searchInputElement.value; //this will give the value
+  console.log(value);
+  if (value !== "") {
+  }
+};
+
+// Method to handle dropdown selection change
+const handleSelectChange = () => {
+  let value = searchInputElement.value;
+  console.log(value);
+  let country = null;
+  for (let i = 0; i < countriesInformation.length; i++) {
+    if (countriesInformation[i].name.common == value) {
+      console.log(countriesInformation[i]);
+      country = countriesInformation[i];
+      break;
     }
-
-
-searchInputElement.addEventListener("change", handleSelectChange)
+  }
+  // Passing country to populateData function
+  populateData(country);
+};
+// Change event listener on select dropdown
+searchInputElement.addEventListener("change", handleSelectChange);
--- js/script.js
+++ js/script.js
@@ -1,166 +1,177 @@
-let tempElement = document.getElementById("temp")
-let locationElement = document.getElementById("location")
-let feelsLikeElement = document.getElementById("feels-like")
-let humidityElement = document.getElementById("humidity")
-let maxtempElement = document.getElementById("max-temp")
-let mintempElement = document.getElementById("min-temp")
-let pressureElement = document.getElementById("pressure")
-let sealevelElement = document.getElementById("sea-level")
-let inputElement = document.getElementById("city-input")
-let searchElement = document.getElementById("search-btn")
-let imageElement = document.getElementById("image")
-let weatherdescriptionElement = document.getElementById("weather-description")
-let forecastElement = document.getElementById("forecast-container")
-var map = L.map('map').setView([0, 0], 1);
-let marker = null
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=p14qZB5wwA8Y5xetwUyR', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-  }).addTo(map);
+// Html elements
+let tempElement = document.getElementById("temp");
+let locationElement = document.getElementById("location");
+let feelsLikeElement = document.getElementById("feels-like");
+let humidityElement = document.getElementById("humidity");
+let maxtempElement = document.getElementById("max-temp");
+let mintempElement = document.getElementById("min-temp");
+let pressureElement = document.getElementById("pressure");
+let sealevelElement = document.getElementById("sea-level");
+let inputElement = document.getElementById("city-input");
+let searchElement = document.getElementById("search-btn");
+let imageElement = document.getElementById("image");
+let weatherdescriptionElement = document.getElementById("weather-description");
+let forecastElement = document.getElementById("forecast-container");
+
+// Adding leaflet.js map
+var map = L.map("map").setView([0, 0], 1);
+let marker = null;
+L.tileLayer(
+  "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=p14qZB5wwA8Y5xetwUyR",
+  {
+    attribution:
+      '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+  }
+).addTo(map);
+
+//   Method to fetch weather information based on location
 let fetchWeatherInfo = async (city) => {
-    
-    //API Call
-    //URL
-    try {
-        let API_KEY = "ca62d981f998767dd6c8395950bb7f3d"
-        let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY
-        console.log(url);
+  //API Call
+  //URL
+  try {
+    // API KEY
+    let API_KEY = "ca62d981f998767dd6c8395950bb7f3d";
+    // API ENDPOINT
+    let url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      API_KEY;
 
-        let response = await fetch(url)
-        let formattedData = await response.json()
-        console.log(formattedData);
-        //If the city is not found...
-        if(formattedData.cod === '404'){
-            alert("City not found")
-        } else { //city found
-   
-    
-    let longitude = formattedData.coord.lon
-    let latitude = formattedData.coord.lat
-    let forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + API_KEY
+    // API CALL
+    let response = await fetch(url);
+    let formattedData = await response.json();
+    console.log(formattedData);
+    //If the city is not found...
+    if (formattedData.cod === "404") {
+      alert("City not found");
+    } else {
+      //city found
 
-    let response2 = await fetch(forecastUrl)
-    let formattedData2 = await response2.json()
+      let longitude = formattedData.coord.lon;
+      let latitude = formattedData.coord.lat;
+      //   Weather forecast API ENDPOINT
+      let forecastUrl =
+        "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+        latitude +
+        "&lon=" +
+        longitude +
+        "&appid=" +
+        API_KEY;
 
-    if (formattedData2.cod === "404") {
-        alert("Location not found")
-        return
-    }
+      let response2 = await fetch(forecastUrl);
+      let formattedData2 = await response2.json();
 
-    let listofForcastweathers = formattedData2.list 
-    console.log(listofForcastweathers);
-    
-    let output = ""
+      if (formattedData2.cod === "404") {
+        alert("Location not found");
+        return;
+      }
 
-    for (let i = 0; i < 20; i++) {
+      let listofForcastweathers = formattedData2.list;
+
+      let output = "";
+      // Looping over forecast weathers array
+      for (let i = 0; i < 20; i++) {
         console.log(listofForcastweathers[i]);
-        let weather = listofForcastweathers[i].main.temp - 273.15
-        let time = listofForcastweathers[i].dt_txt
-        time = time.slice(5,time.length-3)
-        let dateAndTime = time.split(" ")
-        let date = dateAndTime[0]
-        let hourlyTime = dateAndTime[1]
+        let weather = listofForcastweathers[i].main.temp - 273.15;
+        let time = listofForcastweathers[i].dt_txt;
+        time = time.slice(5, time.length - 3);
+        let dateAndTime = time.split(" ");
+        let date = dateAndTime[0];
+        let hourlyTime = dateAndTime[1];
 
-        let iconcode = listofForcastweathers[i].weather[0].icon
+        let iconcode = listofForcastweathers[i].weather[0].icon;
+        // Weather icon
         let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
-        output += "<div class='forecast-card'>  <p class='text-white'>" + date + "</p>  <p class='text-white'>" + hourlyTime + "</p> <img src='"+ iconurl +"'/> <p class='text-white'>" + weather.toFixed(0) + "°C </p></div> "
+        output +=
+          "<div class='forecast-card'>  <p class='text-white'>" +
+          date +
+          "</p>  <p class='text-white'>" +
+          hourlyTime +
+          "</p> <img src='" +
+          iconurl +
+          "'/> <p class='text-white'>" +
+          weather.toFixed(0) +
+          "°C </p></div> ";
+      }
 
+      // Displaying forecast weather
+      forecastElement.innerHTML = output;
+
+      let place = formattedData.name;
+
+      // Displaying location
+      locationElement.innerHTML = place;
+
+      let iconcode = formattedData.weather[0].icon;
+      let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+      // Displaying weather icon
+      imageElement.src = iconurl;
+
+      let weatherdescription = formattedData.weather[0].description;
+      // displaying weather description
+      weatherdescriptionElement.innerHTML = weatherdescription;
+
+      let mainInformation = formattedData.main;
+
+      // Displaying feels like in celsius
+      let feelsLikeInC = mainInformation.feels_like - 273.15;
+      feelsLikeElement.innerHTML = feelsLikeInC.toFixed(0) + "°C";
+
+      // Displaying humidity (in %)
+      let humidity = mainInformation.humidity;
+      humidityElement.innerHTML = humidity.toFixed(0) + "%";
+
+      // Displaying pressure
+      let pressure = mainInformation.pressure;
+      pressureElement.innerHTML = pressure.toFixed(0) + "mb";
+
+      // Displaying sea level
+      let sea_level = mainInformation.sea_level - 273.15;
+      sealevelElement.innerHTML = sea_level.toFixed(0) + "FT";
+
+      // Displaying temperature in celcius
+      let tempInC = mainInformation.temp - 273.15;
+      tempElement.innerHTML = tempInC.toFixed(0) + "°C";
+
+      // Displaying max temperature
+      let maxTempInC = mainInformation.temp_max - 273.15;
+      maxtempElement.innerHTML = maxTempInC.toFixed(0) + "°C";
+
+      // Displaying min temperature in celsius
+      let minTempInC = mainInformation.temp_min - 273.15;
+      mintempElement.innerHTML = minTempInC.toFixed(0) + "°C";
+
+      let timezone = formattedData.timezone;
+      let wind = formattedData.wind;
+      let weatherInfo = formattedData.weather;
+
+      // Removing old marker
+      if (marker != null) {
+        map.removeLayer(marker);
+      }
+      // Jumping to current location
+      map.flyTo([latitude, longitude], 5);
+      marker = L.marker([latitude, longitude]).addTo(map);
     }
-
-    forecastElement.innerHTML = output
-
-     let place = formattedData.name
-
-    locationElement.innerHTML = place
-
-    let iconcode = formattedData.weather[0].icon
-
-    let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-
-    imageElement.src=iconurl
-    
-    let weatherdescription = formattedData.weather[0].description
-
-    weatherdescriptionElement.innerHTML = weatherdescription
-
-    
-    let mainInformation = formattedData.main
-    console.log(mainInformation);
-
-    let feelsLikeInC = mainInformation.feels_like - 273.15
-    feelsLikeElement.innerHTML = feelsLikeInC.toFixed(0) + "°C"
-    console.log(feelsLikeInC);
-
-    let humidity = mainInformation.humidity
-    humidityElement.innerHTML = humidity.toFixed(0) + "%"
-    console.log(humidity);
-
-     // Displaying pressure
-     let pressure = mainInformation.pressure
-     pressureElement.innerHTML = pressure.toFixed(0) + "mb"
-    
-    // Displaying sea level
-    let sea_level = mainInformation.sea_level - 273.15
-    sealevelElement.innerHTML = sea_level.toFixed(0) + "FT"
-
-     // Displaying temperature in celcius
-     let tempInC = mainInformation.temp - 273.15
-     tempElement.innerHTML = tempInC.toFixed(0) + "°C"
-    
-     // Displaying max temperature
-    let maxTempInC = mainInformation.temp_max - 273.15
-    maxtempElement.innerHTML = maxTempInC.toFixed(0) + "°C"
-    console.log(maxTempInC);
-
-
-    let minTempInC = mainInformation.temp_min - 273.15
-    mintempElement.innerHTML = minTempInC.toFixed(0) + "°C"
-    console.log(minTempInC);
-
-
-    let timezone = formattedData.timezone
-    console.log(timezone);
-    let wind = formattedData.wind
-    console.log(wind);
-    let weatherInfo = formattedData.weather
-    console.log(weatherInfo);
-    
-    // Removing old marker
-    if (marker != null) {
-        map.removeLayer(marker)
-    }
-    // Jumping to current location
- //   map.setView([latitude, longitude], 10)
- map.flyTo([latitude, longitude], 5);
- marker = L.marker([latitude, longitude]).addTo(map);
- // map.panTo(latitude, longitude, {
- //   animate: true,
- //   duration: 5,
- // });
-}
-
-} catch(error){
-    console.log("We are inside catch block");
-    console.log(error.message);
+  } catch (error) {
     // if anything goes wrong in the 'try' block,
     // it will jump to catch()
-}
+    console.log(error.message);
+    
+  }
+};
 
-}
-
-fetchWeatherInfo("Stockholm")
+fetchWeatherInfo("Stockholm");
 
 //Change to handleClick()
 const handleButtonClick = () => {
-    let value = inputElement.value //this will give the value
-    console.log(value)
-    if (value !== ""){
-    fetchWeatherInfo(value)
-}
-    
-}
-    
+  let value = inputElement.value; //this will give the value
+  if (value !== "") {
+    fetchWeatherInfo(value);
+  }
+};
 
-
-searchElement.addEventListener("click", handleButtonClick)
-
+// Event listener on search button click
+searchElement.addEventListener("click", handleButtonClick);
